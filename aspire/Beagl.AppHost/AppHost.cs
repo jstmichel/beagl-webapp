e46fda30bfa7aa9bf363226c6c7355b348c9723b
Jonathan St-Michel <j.stmichel@icloud.com>
2025-12-30 13:46:46 -0500

    feat(database): add postgresql support to app host
    * register postgresql database in app host
    * update project file to include postgresql package reference
    
    Closes #15

--- aspire/Beagl.AppHost/AppHost.cs
+++ aspire/Beagl.AppHost/AppHost.cs
@@ -1,11 +1,21 @@
 // MIT License - Copyright (c) 2025 Jonathan St-Michel
 
-IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
+IDistributedApplicationBuilder builder =
+    DistributedApplication.CreateBuilder(args);
 
-builder.AddProject("beagl-webapp", "../../src/Beagl.WebApp");
+// Register PostgreSQL database
+IResourceBuilder<PostgresServerResource> postgresServer = builder.AddPostgres("postgres-server");
+IResourceBuilder<PostgresDatabaseResource> postgresdb = postgresServer.AddDatabase("postgres-db");
+
+// Register Beagl web application
+builder.AddProject("beagl-webapp", "../../src/Beagl.WebApp")
+    .WaitFor(postgresdb)
+    .WithReference(postgresdb);
 
 // builder.AddDockerfile("beagl-webapp-docker", "../../src/Beagl.WebApp")
 //     .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
-//     .WithHttpEndpoint(8080, 8080);
+//     .WithHttpEndpoint(8080, 8080)
+//     .WaitFor(postgresdb)
+//     .WithReference(postgresdb);
 
 builder.Build().Run();
